370b37b8ba10fa4bfe7238781c3eadbfe544ce08
zhs007 <sssxueren@gmail.com>
2019-08-21 11:30:56 +0800

    - updated analyzeimg

--- distanceimg.go
+++ distanceimg.go
@@ -20,14 +20,14 @@ func DistanceColor(c1 color.Color, c2 color.Color) float64 {
 	r1, g1, b1, _ := c1.RGBA()
 	r2, g2, b2, _ := c2.RGBA()
 
-	cf1 := colorful.Color{float64(r1) / 255.0, float64(g1) / 255.0, float64(b1) / 255.0}
-	cf2 := colorful.Color{float64(r2) / 255.0, float64(g2) / 255.0, float64(b2) / 255.0}
+	cf1 := colorful.Color{float64(r1>>8) / 255.0, float64(g1>>8) / 255.0, float64(b1>>8) / 255.0}
+	cf2 := colorful.Color{float64(r2>>8) / 255.0, float64(g2>>8) / 255.0, float64(b2>>8) / 255.0}
 
 	return cf1.DistanceCIE76(cf2)
 }
 
 // DistanceImg - distance image img1 and img2
-func DistanceImg(fn1 string, fn2 string) (image.Image, float64, error) {
+func DistanceImg(fn1 string, fn2 string, mfn string) (image.Image, float64, error) {
 	f1, err := os.Open(fn1)
 	defer f1.Close()
 	if err != nil {
@@ -50,12 +50,27 @@ func DistanceImg(fn1 string, fn2 string) (image.Image, float64, error) {
 		return nil, 0, err
 	}
 
+	mf, err := os.Open(mfn)
+	defer mf.Close()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	mimg, _, err := image.Decode(mf)
+	if err != nil {
+		return nil, 0, err
+	}
+
 	if img1.Bounds().Size().X != img2.Bounds().Size().X || img1.Bounds().Size().Y != img2.Bounds().Size().Y {
 		return nil, 0, brucecorebase.ErrInvalidImageRect
 	}
 
+	if img1.Bounds().Size().X != mimg.Bounds().Size().X || img1.Bounds().Size().Y != mimg.Bounds().Size().Y {
+		return nil, 0, brucecorebase.ErrInvalidImageRect
+	}
+
 	// img := image.NewRGBA(image.Rect(0, 0, img1.Bounds().Size().X, img1.Bounds().Size().Y))
-	img := image.NewGray(image.Rect(0, 0, img1.Bounds().Size().X, img1.Bounds().Size().Y))
+	img := image.NewRGBA(image.Rect(0, 0, img1.Bounds().Size().X, img1.Bounds().Size().Y))
 
 	maxd := DistanceColor(color.RGBA{R: 255, G: 255, B: 255, A: 255}, color.RGBA{R: 0, G: 0, B: 0, A: 0})
 
@@ -65,19 +80,125 @@ func DistanceImg(fn1 string, fn2 string) (image.Image, float64, error) {
 		for x := 0; x < img1.Bounds().Size().X; x++ {
 			c1 := img1.At(x, y)
 			c2 := img2.At(x, y)
+			mc := mimg.At(x, y)
 
 			d := DistanceColor(c1, c2)
 			if d < 0 {
 				d = -d
 			}
 
-			lst = append(lst, d/maxd)
+			r1, g1, b1, _ := c1.RGBA()
 
-			img.SetGray(x, y, color.Gray{Y: uint8(d / maxd * 255.0)})
+			_, _, _, ma := mc.RGBA()
+
+			// cf := colorful.Color{float64(r1>>8) / 255.0, float64(g1>>8) / 255.0, float64(b1>>8) / 255.0}
+			// _, _, v1 := cf.Hsv()
+
+			lst = append(lst, d*float64(ma>>8)/255.0)
+
+			// r1, g1, b1, _ := c1.RGBA()
+
+			img.Set(x, y, color.RGBA{
+				R: uint8(r1 >> 8),
+				G: uint8(g1 >> 8),
+				B: uint8(b1 >> 8),
+				A: uint8(d * 255.0 / maxd),
+			})
+
+			// img.Set(x, y, color.RGBA{
+			// 	R: uint8(d * 255.0 / maxd),
+			// 	G: uint8(d * 255.0 / maxd),
+			// 	B: uint8(d * 255.0 / maxd),
+			// 	A: uint8(d * 255.0 / maxd),
+			// })
 		}
 	}
 
 	v := stat.Variance(lst, nil)
 
 	return img, v, nil
+}
+
+// GetSaturation - get saturation and variance
+func GetSaturation(fn string) (image.Image, float64, error) {
+	f, err := os.Open(fn)
+	defer f.Close()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// img := image.NewRGBA(image.Rect(0, 0, img1.Bounds().Size().X, img1.Bounds().Size().Y))
+	imgret := image.NewGray(image.Rect(0, 0, img.Bounds().Size().X, img.Bounds().Size().Y))
+
+	// maxd := DistanceColor(color.RGBA{R: 255, G: 255, B: 255, A: 255}, color.RGBA{R: 0, G: 0, B: 0, A: 0})
+
+	var lst []float64
+
+	for y := 0; y < img.Bounds().Size().Y; y++ {
+		for x := 0; x < img.Bounds().Size().X; x++ {
+			c := img.At(x, y)
+
+			r, g, b, _ := c.RGBA()
+
+			cf := colorful.Color{float64(r>>8) / 255.0, float64(g>>8) / 255.0, float64(b>>8) / 255.0}
+			_, s, _ := cf.Hsv()
+
+			lst = append(lst, s)
+
+			imgret.SetGray(x, y, color.Gray{
+				Y: uint8(s * 255.0),
+			})
+		}
+	}
+
+	v := stat.Variance(lst, nil)
+
+	return imgret, v, nil
+}
+
+// GetLightness - get lightness and variance
+func GetLightness(fn string) (image.Image, float64, error) {
+	f, err := os.Open(fn)
+	defer f.Close()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// img := image.NewRGBA(image.Rect(0, 0, img1.Bounds().Size().X, img1.Bounds().Size().Y))
+	imgret := image.NewGray(image.Rect(0, 0, img.Bounds().Size().X, img.Bounds().Size().Y))
+
+	// maxd := DistanceColor(color.RGBA{R: 255, G: 255, B: 255, A: 255}, color.RGBA{R: 0, G: 0, B: 0, A: 0})
+
+	var lst []float64
+
+	for y := 0; y < img.Bounds().Size().Y; y++ {
+		for x := 0; x < img.Bounds().Size().X; x++ {
+			c := img.At(x, y)
+
+			r, g, b, _ := c.RGBA()
+
+			cf := colorful.Color{float64(r>>8) / 255.0, float64(g>>8) / 255.0, float64(b>>8) / 255.0}
+			_, _, v := cf.Hsv()
+
+			lst = append(lst, v)
+
+			imgret.SetGray(x, y, color.Gray{
+				Y: uint8(v * 255.0),
+			})
+		}
+	}
+
+	v := stat.Variance(lst, nil)
+
+	return imgret, v, nil
 }
--- samples/analyzeimg/main.go
+++ samples/analyzeimg/main.go
@@ -23,15 +23,45 @@ func img2webp(fn string, outputfn string, Lossless bool, q float32) error {
 	return nil
 }
 
-func distanceimg(fn1 string, fn2 string, ofn string) error {
-	img, v, err := brucecore.DistanceImg(fn1, fn2)
+func distanceimg(fn1 string, fn2 string, mfn string, ofn string) error {
+	img, v, err := brucecore.DistanceImg(fn1, fn2, mfn)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("v = %v", v)
 
-	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	f, _ := os.OpenFile(ofn, os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, img)
+
+	return nil
+}
+
+func savesaturationimg(fn string, ofn string) error {
+	img, v, err := brucecore.GetSaturation(fn)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("v = %v", v)
+
+	f, _ := os.OpenFile(ofn, os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, img)
+
+	return nil
+}
+
+func savelightnessimg(fn string, ofn string) error {
+	img, v, err := brucecore.GetLightness(fn)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("v = %v", v)
+
+	f, _ := os.OpenFile(ofn, os.O_WRONLY|os.O_CREATE, 0600)
 	defer f.Close()
 	png.Encode(f, img)
 
@@ -39,19 +69,29 @@ func distanceimg(fn1 string, fn2 string, ofn string) error {
 }
 
 func main() {
-	err := img2webp("./test001.png", fmt.Sprintf("./test001-l.webp"), true, 0)
-	if err != nil {
-		fmt.Printf("img2webp error %v", err)
-	}
+	// err := img2webp("./test001.png", fmt.Sprintf("./test001-l.webp"), true, 0)
+	// if err != nil {
+	// 	fmt.Printf("img2webp error %v", err)
+	// }
 
-	for i := 100; i > 0; i -= 10 {
-		err := img2webp("./test001.png", fmt.Sprintf("./test001-q%v.webp", i), false, float32(i))
-		if err != nil {
-			fmt.Printf("img2webp error %v", err)
-		}
-	}
+	// for i := 100; i > 0; i -= 10 {
+	// 	err := img2webp("./test001.png", fmt.Sprintf("./test001-q%v.webp", i), false, float32(i))
+	// 	if err != nil {
+	// 		fmt.Printf("img2webp error %v", err)
+	// 	}
+	// }
 
-	err = distanceimg("./test001.png", "./test001-q10.webp", "./output.png")
+	// err := savesaturationimg("./test001.png", "./outs.png")
+	// if err != nil {
+	// 	fmt.Printf("savesaturationimg error %v", err)
+	// }
+
+	// err := savelightnessimg("./test001.png", "./outv.png")
+	// if err != nil {
+	// 	fmt.Printf("savesaturationimg error %v", err)
+	// }
+
+	err := distanceimg("./test001.png", "./test001-tinypng.png", "./test001-mask.png", "./output.png")
 	if err != nil {
 		fmt.Printf("img2webp error %v", err)
 	}
